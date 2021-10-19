def50b1f92d9065602daebf99d51638bac4e4815
Fausnightm <m.fausnight@lafilm.edu>
2021-10-19 19:56:30 -0400

    fixed server error

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+// import ApolloClient from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-  uri: "/graphql",
+const httpLink = createHttpLink({
+  uri: '/graphql',
 });
+
+const authLink = setContext(()=> {
+    const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  }
+})
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
 
 function App() {
   return (
